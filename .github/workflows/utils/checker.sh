d11be16a3bab5a8b664aa26a8a23d4c33aa09dca
Rhys Deimel <deimelr@hotmail.com>
2024-11-09 13:24:35 +1100

    test (#3)
    * test
    
    * moving data into checker
    
    * test
    
    * test
    
    * test
    
    * update
    
    * test
    
    * test
    
    * test
    
    * update
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * update
    
    * test
    
    * update
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test

---
+++ .github/workflows/utils/checker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# get general job data
+branch_sha=$(echo -n "$BRANCH_NAME" | sha256sum | head -c 7)
+echo "branch_sha=$branch_sha"
+echo "branch_sha=$branch_sha" >> $GITHUB_OUTPUT
+echo "**branch_sha**: \`$branch_sha\`" >> $GITHUB_STEP_SUMMARY
+
+workload_version=$(cat "$VERSION_FILE_PATH" | cut -d "'" -f 2)
+echo "workload_version=$workload_version"
+echo "workload_version=$workload_version" >> $GITHUB_OUTPUT
+echo "**workload_version**: \`$workload_version\`" >> $GITHUB_STEP_SUMMARY
+
+image_tag="$workload_version-$branch_sha"
+echo "image_tag=$image_tag"
+echo "image_tag=$image_tag" >> $GITHUB_OUTPUT
+echo "**image_tag**: \`$image_tag\`" >> $GITHUB_STEP_SUMMARY
+
+
+# get changes
+change_schema="./.github/workflows/utils/change_schema.yml"
+# load yml array into a bash array
+# need to output each entry as a single line
+readarray pipelines < <(yq e -o=j -I=0 '.pipelines[]' $change_schema )
+
+for item in "${pipelines[@]}"; do
+    # item is a yaml snippet representing a single entry
+    name=$(echo "$item" | yq e '.name' -)
+    regex=$(echo "$item" | yq e '.regex' -)
+    echo "name: $name"
+    echo "regex: $regex"
+
+    # Grep with -e
+    # This causes the run to terminate immediately when any pipeline exits with a non-zero status.
+    # grep returns a 1 when it doesn't find any match, and thus, terminates immediately.
+    # We instead run an OR on the grep command, and exit on anything greater than 1 so we don't
+    # clobber other error codes.
+    exit_code=0
+    result=$(git diff $BASE $COMPARE --name-only | grep -P -c "$regex" || exit_code=$?)
+    echo $result
+    if (( exit_code > 1 )) ; then
+        exit $exit_code
+    fi
+
+    echo "$name=$result" >> $GITHUB_OUTPUT
+done
---
+++ .github/workflows/utils/pr_comment.js
@@ -0,0 +1,40 @@
+module.exports = async ({github, context, core}) => {
+    const {BRANCH_SHA} = process.env
+    const {WORKLOAD_VERSION} = process.env
+    const {IMAGE_TAG} = process.env
+
+    // 1. Retrieve existing bot comments for the PR
+    const {data: comments} = await github.rest.issues.listComments({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+    })
+    const botComment = comments.find(comment => {
+        return comment.user.type === 'Bot' && comment.body.includes('Pipeline Data 📋️')
+    })
+
+    // Not using multiline template literal because actions turns it into a code block
+    const output = '#### Pipeline Data 📋️' +
+        '\n' +
+        `Branch sha: \`${BRANCH_SHA}\`\n` +
+        `Workload version: \`${WORKLOAD_VERSION}\`\n` +
+        `Image tag: \`${IMAGE_TAG}\`\n`
+    ;
+
+// 3. If we have a comment, update it, otherwise create a new one
+    if (botComment) {
+        github.rest.issues.updateComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            comment_id: botComment.id,
+            body: output
+        })
+    } else {
+        github.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: output
+        })
+    }
+}

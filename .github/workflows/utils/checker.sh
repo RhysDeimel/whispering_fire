6d6c6f64be1d8a747641c67b00bc586cd7ae1f25
Rhys <deimelr@hotmail.com>
2024-11-09 12:46:35 +1100

    update

--- .github/workflows/utils/checker.sh
+++ .github/workflows/utils/checker.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
 
-# get general job data
-branch_sha=$(echo -n "$BRANCH_NAME" | sha256sum | head -c 7)
-echo "branch_sha=$branch_sha"
-echo "branch_sha=$branch_sha" >> $GITHUB_OUTPUT
-echo "**branch_sha**: \`$branch_sha\`" >> $GITHUB_STEP_SUMMARY
+set -euxo pipefail
+
+## get general job data
+#branch_sha=$(echo -n "$BRANCH_NAME" | sha256sum | head -c 7)
+#echo "branch_sha=$branch_sha"
+#echo "branch_sha=$branch_sha" >> $GITHUB_OUTPUT
+#echo "**branch_sha**: \`$branch_sha\`" >> $GITHUB_STEP_SUMMARY
+#
+#
+#workload_version=$(cat "$VERSION_FILE_PATH" | cut -d "'" -f 2)
+#echo "workload_version=$workload_version"
+#echo "workload_version=$workload_version" >> $GITHUB_OUTPUT
+#echo "**workload_version**: \`$workload_version\`" >> $GITHUB_STEP_SUMMARY
+#
+#image_tag="$workload_version-$branch_sha"
+#echo "image_tag=$image_tag"
+#echo "image_tag=$image_tag" >> $GITHUB_OUTPUT
+#echo "**image_tag**: \`$image_tag\`" >> $GITHUB_STEP_SUMMARY
 
 
-workload_version=$(cat "$VERSION_FILE_PATH" | cut -d "'" -f 2)
-echo "workload_version=$workload_version"
-echo "workload_version=$workload_version" >> $GITHUB_OUTPUT
-echo "**workload_version**: \`$workload_version\`" >> $GITHUB_STEP_SUMMARY
+# get changes
+change_schema="./.github/workflows/utils/change_schema.yml"
 
-image_tag="$workload_version-$branch_sha"
-echo "image_tag=$image_tag"
-echo "image_tag=$image_tag" >> $GITHUB_OUTPUT
-echo "**image_tag**: \`$image_tag\`" >> $GITHUB_STEP_SUMMARY
+# load yml array into a bash array
+# need to output each entry as a single line
+readarray pipelines < <(yq e -o=j -I=0 '.pipelines[]' $change_schema )
 
-# get application code changes
+for item in "${pipelines[@]}"; do
+    # item is a yaml snippet representing a single entry
+    name=$(echo "$item" | yq e '.name' -)
+    regex=$(echo "$item" | yq e '.regex' -)
+    echo "name: $name"
+    echo "regex: $regex"
 
-# get IaC changes
+    base="master"
+    compare="HEAD"
 
-# get doc changes
+    # Grep with -e
+    # This causes the run to terminate immediately when any pipeline exits with a non-zero status.
+    # grep returns a 1 when it doesn't find any match, and thus, terminates immediately.
+    # We instead run an OR on the grep command, and exit on anything greater than 1 so we don't
+    # clobber other error codes.
+    echo "git diff $base $compare --name-only | grep -P -c '$regex'"
+    exit_code=0
+    result=$(git diff $base $compare --name-only | grep -P -c '$regex' || exit_code=$?)
+    if (( exit_code > 1 )) ; then
+        exit $exit_code
+    fi
 
-# get test changes
-
-# get config changes
-
-
+    echo $name = $result
+    echo
+done

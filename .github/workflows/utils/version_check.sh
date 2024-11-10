f19e74f85a6987680fa20db6661de6aa15b9dadb
Rhys <deimelr@hotmail.com>
2024-11-10 16:16:54 +1100

    test

--- .github/workflows/utils/version_check.sh
+++ .github/workflows/utils/version_check.sh
@@ -5,8 +5,8 @@ set -euxo pipefail
 # Check semver by turning it into a 7 digit int for comparison
 function semver_to_int { echo "$@" | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }'; }
 
-compare_semver=$(head "$VERSION_FILE_PATH" -n 1 | cut -d "'" -f 2)
-base_semver=$(head "$VERSION_FILE_PATH" -n 1 | cut -d "'" -f 2)
+compare_semver=$(head "compare/$VERSION_FILE_PATH" -n 1 | cut -d "'" -f 2)
+base_semver=$(head "base/$VERSION_FILE_PATH" -n 1 | cut -d "'" -f 2)
 
 if [ "$(semver_to_int "$base_semver")" -ge "$(semver_to_int "$compare_semver")" ]; then
     echo -e "\e[31;1mError:\e[0m Version needs incrementing!"
